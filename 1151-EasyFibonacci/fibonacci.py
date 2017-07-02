784e18736e241f1c19172c3a5aa69e2b22de86d0
Dalton Lima <daltonvarussa@gmail.com>
2017-07-02 20:05:09 -0300

    Solved Easy Fibonacci #1151

---
+++ 1151-EasyFibonacci/fibonacci.py
@@ -0,0 +1,24 @@
+# Easy Fibonnaci #1151
+# Dalton Lima 07/02/17 
+# More problems @github.com/daltonbr/uri
+
+import sys
+
+user_input = input()
+
+total_terms = int(user_input)
+term = 0
+next_term = 1
+
+print(term, end=' ')
+if (total_terms == 1):
+	sys.exit()
+
+for i in range(1, total_terms-1):
+	print(next_term, end=' ')
+	aux = next_term
+	next_term += term
+	term = aux
+
+# last term doesn't have a space after it (as requested)
+print(next_term)
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # uri
 a repo for general problems and chalenges from www.urionlinejudge.com.br
 
-Solved: 1017, 1089, 2203
+Solved: 1017, 1089, 1151, 2203
