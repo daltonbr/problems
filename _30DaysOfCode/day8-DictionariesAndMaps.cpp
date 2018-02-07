3c9c1870d929feeb4f88de8f874e20f309ed9a06
Dalton Lima <daltonvarussa@gmail.com>
2018-02-07 14:56:13 -0200

    day8 - Dictionaries and Maps

--- _30DaysOfCode/day8-DictionariesAndMaps.cpp
+++ _30DaysOfCode/day8-DictionariesAndMaps.cpp
@@ -2,47 +2,46 @@
 https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
 */
 
-#include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
-#include <algorithm>
 #include <unordered_map>
 using namespace std;
 
-int main() {
+
+int main()
+{
     int n; cin >> n;
-    std::unordered_map<string, string> phoneBook =
-    {
-        {"Dalton","234-5678"},
-        {"Carmack","555-5555"}
-    };
+    std::unordered_map<string, string> phoneBook;
 
     // reading entries
-    // string name, phone;
-    // for (int i = 0; i < n-1; i++)
-    // {
-    //     cin >> name >> phone;
-    //     phoneBook.insert(name, phone);        
-    // }
+    std::string name, phone;
+    for (int i = n-1; i >= 0; i--)
+    {        
+       cin >> name;       
+       getchar(); // blank space
+       cin >> phone;
+       phoneBook[name] = phone;
+    }
 
     // Iterate and print keys and values of unordered_map
-    for( const auto& n : phoneBook ) {
-        std::cout << "Key:[" << n.first << "] Value:[" << n.second << "]\n";
+    // for( const auto& n : phoneBook ) {
+    //     std::cout << "Key:[" << n.first << "] Value:[" << n.second << "]\n";
+    // }
+    
+    string queryInput;
+    // queries
+    while(cin >> queryInput)
+    {
+        auto search = phoneBook.find(queryInput);
+        if (search != phoneBook.end())
+        {
+            cout << search->first << "=" << search->second << endl;
+        }
+        else
+        {
+            cout << "Not found" << endl;
+        }
     }
- 
-    // Add two new entries to the unordered_map
-    phoneBook["BLACK"] = "#000000";
-    phoneBook["WHITE"] = "#FFFFFF";
- 
-    // Output values by key
-    std::cout << "The HEX of color RED is:[" << phoneBook["RED"] << "]\n";
-    std::cout << "The HEX of color BLACK is:[" << phoneBook["BLACK"] << "]\n";
-
-    // printing again
-    for( const auto& n : phoneBook ) {
-        std::cout << "Key:[" << n.first << "] Value:[" << n.second << "]\n";
-    }
-
     return 0;
 }
