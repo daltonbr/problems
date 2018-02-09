a41009b6c42e1719045c304c6f8b9c584a52a6d6
Dalton Lima <daltonvarussa@gmail.com>
2018-02-09 20:13:06 -0200

    Virtual Functions

--- _HackerRank/virtualFunctions.cpp
+++ _HackerRank/virtualFunctions.cpp
@@ -14,74 +14,66 @@ using namespace std;
 //Solve this problem using virtual functions, constructors and static variables.
 // You can create more data members if you want.
 
-//Person::id = 0;
+/*c++17 inline*/const int MARKS_ARRAY_SIZE = 6;
 
 class Person
 {
-    //int Person::id = 0;
-
-    protected:        
+    protected:
+        int cur_id;
         string name;
         int age;
     public:
-        //Person();
-        virtual void getdata()
-        { cout<< "getdata base class" <<endl; }
-
-        virtual void putdata()
-        { cout<< "putdata base class" <<endl; }
+        virtual void getdata() {};
+        virtual void putdata() {};
 };
 
 class Professor : public Person
-{    
+{
     private:        
         int publications;
-        int cur_id;
+        static int id;
 
     public:
-        Professor()
-        {
-            cout << "Professor created" << endl;
-        }
+        Professor() { cur_id = id++; }
 
         Professor (string _name, int _age, int _publications)
         {
             name = _name;
             age = _age;
-            publications = _publications;
-            //cur_id = Person::id;
+            publications = _publications;            
         }
 
+        // get name, age and publications
         void getdata()
         {
-            cout<< "getdata Professor class: " <<endl;
             cin >> name;
             cin >> age;
             cin >> publications;
-            // get name, age and publications
         }
 
+        // print name, age, publications and cur_id
         void putdata()
         {
-            // print name, age, publications and cur_id                        
-            cout << "putdata Professor class" << endl;                            
-            cout << name << " " << age << " " << publications << endl;
-            //cout << name << endl;
+            cout << name << " " << age << " " << publications << " " << cur_id << endl;            
         }
-
 };
 
 class Student : public Person
 {
-    //int marks[]; // array of size 6. type?
     private:
-       int cur_i;
+        int marks[MARKS_ARRAY_SIZE];
+        static int id;
+
+        int marksSum()
+        {
+            int sum = 0;
+            for (int i = 0; i < MARKS_ARRAY_SIZE; i++)
+            { sum += marks[i]; }
+            return sum;
+        }
 
     public:
-        Student()
-        {
-            cout << "Student created" << endl;
-        }
+        Student() { cur_id = id++; }
 
         Student (string _name, int _age)
         {
@@ -89,67 +81,49 @@ class Student : public Person
             _age = age;
         }
 
+        // get name, age and the marks
         void getdata()
-            {
-                // get name, age and the marks
-                cout<< "getdata Person class" <<endl;
-                cin >> name;
-                cin >> age;                
-            }
-            void putdata()
-            {
-                //print name, age, sum of the marks and the cur_id            
-                cout << "putdata Student class" << endl;
-                cout << name << " " << age << endl;
-            }
+        {
+            cin >> name;
+            cin >> age;
+            for (int i = 0; i < MARKS_ARRAY_SIZE; i++) { cin >> marks[i]; }
+        }
+
+        //print name, age, sum of the marks and the cur_id
+        void putdata()
+        {
+            cout << name << " " << age << " " << marksSum() << " " << cur_id << endl;
+        }
 };
 
+// Initializing static variables (must be outside of the class)
+int Professor::id = 1;
+int Student::id = 1;
+
 // This code bellow is supplied and "locked"
-// int main(){
+int main(){
 
-//     int n, val;
-//     cin>>n; //The number of objects that is going to be created.
-//     Person *per[n];
+    int n, val;
+    cin>>n; //The number of objects that is going to be created.
+    Person *per[n];
 
-//     for(int i = 0;i < n;i++){
+    for(int i = 0;i < n;i++){
 
-//         cin>>val;
-//         if(val == 1){
-//             // If val is 1 current object is of type Professor
-//             per[i] = new Professor;
+        cin>>val;
+        if(val == 1){
+            // If val is 1 current object is of type Professor
+            per[i] = new Professor;
 
-//         }
-//         else per[i] = new Student; // Else the current object is of type Student
+        }
+        else per[i] = new Student; // Else the current object is of type Student
 
-//         per[i]->getdata(); // Get the data from the user.
+        per[i]->getdata(); // Get the data from the user.
 
-//     }
+    }
 
-//     for(int i=0;i<n;i++)
-//         per[i]->putdata(); // Print the required output for each object.
+    for(int i=0;i<n;i++)
+        per[i]->putdata(); // Print the required output for each object.
 
-//     return 0;
-
-// }
-
-int main()
-{
-    Person *Prof;
-    Person *Stud;
-
-    Prof = new Professor("Dalton", 35, 0);
-    Stud = new Student("Jesse", 18);
-
-    //Prof = new Professor;
-    //Stud = new Student;
-
-    Prof->putdata();
-    Stud->putdata();
-
-    Prof->getdata();
-    Prof->putdata();
-
-    Stud->getdata();
-    Stud->putdata();
     return 0;
+
 }
