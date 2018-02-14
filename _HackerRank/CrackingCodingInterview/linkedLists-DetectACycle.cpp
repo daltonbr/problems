0568b9ee1bae304b411fd451a67b1f1ccb14196d
Dalton Lima <daltonvarussa@gmail.com>
2018-02-14 17:13:48 -0200

    [wip] linkedList Cycle

---
+++ _HackerRank/CrackingCodingInterview/linkedLists-DetectACycle.cpp
@@ -0,0 +1,22 @@
+/*
+Linked Lists: Detect a Cycle
+https://www.hackerrank.com/challenges/ctci-linked-list-cycle/problem
+
+This is NOT A STANDALONE TEST
+Our hidden code checker passes the appropriate argument to your function.
+You are not responsible for reading any input from stdin.
+
+Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
+
+A Node is defined as: 
+    struct Node {
+        int data;
+        struct Node* next;
+    }
+*/
+
+bool has_cycle(Node* head)
+{
+    // Complete this function
+    // Do not write the main method
+}
