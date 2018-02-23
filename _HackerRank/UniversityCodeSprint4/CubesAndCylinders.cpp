a4764afa038ce3a13afbdb65018b722f836e50d9
daltonbr <daltonvarussa@gmail.com>
2018-02-23 17:14:17 +0000

    [wip]

---
+++ _HackerRank/UniversityCodeSprint4/CubesAndCylinders.cpp
@@ -0,0 +1,26 @@
+/*
+Cubes and Cylinders
+https://www.hackerrank.com/contests/university-codesprint-4/challenges/cubes-and-cylinders
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void maximumPackages(vector<int>s, vector<int>k, vector<int>r. vector<int>c)
+{
+
+}
+
+int main()
+{
+    int n, m; cin >> n >> m;
+    vector<int> s, k, r, c;
+
+    
+    
+    return 0;
+}
