4fc9244b981a075ff44ca0d1a05f6ee8d321bf9f
Dalton Lima <daltonvarussa@gmail.com>
2018-03-03 11:28:05 -0300

    python #hackerrank

---
+++ _HackerRank/Python/findThePercentage.py
@@ -0,0 +1,21 @@
+#https://www.hackerrank.com/challenges/finding-the-percentage/problem
+
+from functools import reduce
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    
+    average = sum(student_marks[query_name]) / len(student_marks[query_name])
+    
+    # other way
+    #average2 = reduce(lambda x, y: x+y, student_marks[query_name]) / len(student_marks[query_name])
+    
+    print ("%.2f" % average)    
+    
